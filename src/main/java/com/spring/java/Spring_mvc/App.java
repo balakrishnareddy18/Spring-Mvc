2066bbbadca7fed06f6e1051aab9cb6284ea0bbd
balu <150201543@svuca.edu>
2017-07-09 16:08:22 -0500

    initial commit

---
+++ src/main/java/com/spring/java/Spring_mvc/App.java
@@ -0,0 +1,13 @@
+package com.spring.java.Spring_mvc;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
